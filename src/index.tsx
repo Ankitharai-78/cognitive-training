0a8f976216ec9302b9df7751601118d1fc164b47
Ankitharai-78 <ankitharaik78@gmail.com>
2022-05-01 09:55:05 +0530

    session10

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const name = "Ankitha Rai";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App firstName={name} age={21}/>
+    <App />
   </React.StrictMode>
 );
 
